a2a6b2e6ab49bc974cc0ee3cd02958cff7df1420
Sam. K. <sam.ds@live.com>
2025-08-14 17:33:41 +0200

    adzuna request sender

--- src/DataCollector/RequestSenders/AdzunaRequestSender.js
+++ src/DataCollector/RequestSenders/AdzunaRequestSender.js
@@ -1,65 +1,45 @@
 const axios = require('axios');
 const Logger = require("../Loggers/CollectorLogger");
+const AdzunaCollectionError = require("../Errors/AdzunaCollectionError");
+const {getCountryCode} = require("../Utils/LanguageUtils");
 
 class AdzunaRequestSender {
     static DATA_PROVIDER = "Adzuna";
     static API_URL = "https://api.adzuna.com/v1/api/jobs";
     static APP_ID = process.env.ADZUNA_APP_ID;
-    static API_KEY = process.env.ADZUNA_API_KEY;
 
     constructor(apiKey) {
-        this.apiKey = apiKey;
+        this.API_KEY = apiKey;
     }
 
     setApiKey(apiKey) {
-        this.apiKey = apiKey;
+        this.API_KEY = apiKey;
     }
 
     buildParams(jobType, requestedPage, options = {}) {
         const {
-            country = "it",
+            country = getCountryCode(process.env.API_LANGUAGE),
             location = "",
-            salaryMin = "",
-            salaryMax = "",
-            sortBy = "",
-            resultsPerPage = "",
-            distance = "",
-            company = ""
         } = options;
 
         const params = {
             app_id: AdzunaRequestSender.APP_ID,
-            app_key: AdzunaRequestSender.API_KEY
+            app_key: this.API_KEY
         };
 
-        if (requestedPage) params.page = requestedPage;
         if (jobType) params.what = jobType;
         if (location) params.where = location;
-        if (salaryMin) params.salary_min = salaryMin;
-        if (salaryMax) params.salary_max = salaryMax;
-        if (sortBy) params.sort_by = sortBy;
-        if (resultsPerPage) params.results_per_page = resultsPerPage;
-        if (distance) params.distance = distance;
-        if (company) params.company = company;
 
         return { params, country };
     }
 
     async sendRequest(jobType, requestedPage = 1, options = {}) {
         const { params, country } = this.buildParams(jobType, requestedPage, options);
-        const url = `${AdzunaRequestSender.API_URL}/${country}/search/${requestedPage}`;
+        const url = `${AdzunaRequestSender.API_URL}/${country}/search/${requestedPage}/?${new URLSearchParams(params)}`;
 
         try {
             Logger.debug(`${AdzunaRequestSender.DATA_PROVIDER}: Sending request to: ${url}`);
-            Logger.debug(`${AdzunaRequestSender.DATA_PROVIDER}: With params: ${JSON.stringify(params)}`);
-
-            const response = await axios.get(url, {
-                params: params,
-                headers: {
-                    'Accept': 'application/json'
-                }
-            });
-
+            const response = await axios.get(url);
             return this.formatResponse(response?.data, jobType, params.where, country);
         } catch (error) {
             console.log(error)
@@ -82,13 +62,14 @@ class AdzunaRequestSender {
     }
 
     formatError(error, jobType, requestedPage) {
-        return new Error(`${AdzunaRequestSender.DATA_PROVIDER} Error: ${error.message}`, {
+        return new AdzunaCollectionError(`${AdzunaRequestSender.DATA_PROVIDER} Error: ${error.message}`, {
             status: error?.response?.status,
             jobType: jobType,
             requestedPage: requestedPage,
             originalError: error
         });
     }
+
 }
 
 module.exports = AdzunaRequestSender;
